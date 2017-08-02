58c53c8385b6eb2a5a0a716256eadd4adba20edb
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2017-08-02 19:37:13 +0200

    WIP, working

--- skejbyterning-ng/src/app/app-routing.module.ts
+++ skejbyterning-ng/src/app/app-routing.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RulesComponent } from './rules/rules.component';
+import { GameComponent } from './game/game.component';
+import { AboutComponent } from './about/about.component';
+import { DonateComponent } from './donate/donate.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    children: []
-  }
+  {path: '', component: GameComponent},
+  {path: 'rules', component: RulesComponent},
+  {path: 'game', component: GameComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'donate', component: DonateComponent},
+  {path: '**', component: GameComponent}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- skejbyterning-ng/src/app/app.module.ts
+++ skejbyterning-ng/src/app/app.module.ts
@@ -1,12 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AboutComponent } from './about/about.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DonateComponent } from './donate/donate.component';
+import { GameComponent } from './game/game.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RulesComponent } from './rules/rules.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    RulesComponent,
+    GameComponent,
+    AboutComponent,
+    DonateComponent
   ],
   imports: [
     BrowserModule,
@@ -15,4 +25,5 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
