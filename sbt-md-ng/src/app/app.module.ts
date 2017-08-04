e22437f31f12f03eb87eda7a5b1c5d9e249d2b29
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2017-08-04 16:28:59 +0200

    Added some components to material module and moved md browermodule into this

--- sbt-md-ng/src/app/app.module.ts
+++ sbt-md-ng/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,9 +11,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- sbt-md-ng/src/app/material/material.module.ts
+++ sbt-md-ng/src/app/material/material.module.ts
@@ -1,9 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MdButtonModule, MdCheckboxModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MdButtonModule,
+    MdCheckboxModule,
+    BrowserAnimationsModule
+  ],
+  exports: [
+    MdButtonModule,
+    MdCheckboxModule,
+    BrowserAnimationsModule
   ],
   declarations: []
 })
