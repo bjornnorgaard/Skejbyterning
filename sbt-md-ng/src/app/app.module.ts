b77db07d5bf1a7c4f50f91f7a16806ce367db332
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2017-08-06 01:58:04 +0200

    Setting up firebase

--- sbt-md-ng/src/app/app.module.ts
+++ sbt-md-ng/src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
+import { AngularFireModule } from 'angularfire2';
+
 import 'hammerjs';
+import json from '../../very-secret-file.json';
 import { AboutModule } from './about/about.module';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { GameModule } from './game/game.module';
 import { MyMaterialModule } from './my-material/my-material.module';
 import { RulesModule } from './rules/rules.module';
+
+export const firebaseConfig = {
+  apiKey: json.apiKey,
+  authDomain: json.authDomain,
+  databaseURL: json.databaseURL,
+  storageBucket: json.storageBucket,
+  messagingSenderId: json.messagingSenderId,
+};
 
 @NgModule({
   declarations: [
@@ -19,6 +31,7 @@ import { RulesModule } from './rules/rules.module';
     AppRoutingModule,
     HttpModule,
     MyMaterialModule,
+    AngularFireModule.initializeApp(firebaseConfig),
     GameModule,
     RulesModule,
     AboutModule,
