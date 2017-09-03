a7c0763dbc1ad010f529ab4c1ca259b8a8648dfb
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2017-09-03 17:34:42 +0200

    Added projects for models, interfaces and engine itself

---
+++ GameEngine/Engine.Game/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Engine.Game
+{
+    public class Class1
+    {
+    }
+}
---
+++ GameEngine/Engine.Model/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Engine.Model
+{
+    public class Class1
+    {
+    }
+}
